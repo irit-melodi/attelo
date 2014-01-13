3fd5b4ffb16a5598e6bcdfbde0afb810dec1fc86
Eric Kow <eric.kow@gmail.com>
2014-01-13 18:14:32 +0000

    Setup scripts

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+      name     = "attelo",
+      version  = "0.2",
+      author   = "Philippe Muller, Stergos Afantenos, Pascal Denis",
+      author_email = "Philippe.Muller@irit.fr",
+      packages = find_packages(exclude=["scripts","experiments","tests"]),
+      scripts  = [ "scripts/attelo" ]
+);
