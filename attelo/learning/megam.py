db6ff42476d2a0fb546f9287ab8da2d40021ab39
Eric Kow <eric.kow@gmail.com>
2014-08-01 11:34:27 +0100

    Avoid "Found megam" output in stdout

--- attelo/learning/megam.py
+++ attelo/learning/megam.py
@@ -13,10 +13,30 @@ import sys
 import Orange
 
 
+# nice trick from http://stackoverflow.com/a/6796752
+class RedirectStdStreams(object):
+    def __init__(self, stdout=None, stderr=None):
+        self._stdout = stdout or sys.stdout
+        self._stderr = stderr or sys.stderr
+
+    def __enter__(self):
+        self.old_stdout, self.old_stderr = sys.stdout, sys.stderr
+        self.old_stdout.flush()
+        self.old_stderr.flush()
+        sys.stdout, sys.stderr = self._stdout, self._stderr
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._stdout.flush()
+        self._stderr.flush()
+        sys.stdout = self.old_stdout
+        sys.stderr = self.old_stderr
+
+
 try:
     import nltk.classify.megam as megam
     import nltk.classify.maxent as maxent
-    megam.config_megam()
+    with RedirectStdStreams(stdout=stderr):
+        megam.config_megam()
     #megam.config_megam(bin="path/to/megam")
     use_megam = True
 except:
