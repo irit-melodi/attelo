ca0da56a1f368f93a8868ad6797e5c87919ca9e3
Eric Kow <eric.kow@gmail.com>
2014-08-01 10:31:33 +0100

    irit-rst-dt clean: remove intermediary eval files
    Kill:
    - any scratch directories
    - any incomplete eval dirs
    
    It's also tempting to get rid of feature directories without
    evaluations but it's not likely to be a common case in future
    development

--- irit_rst_dt/cmd/__init__.py
+++ irit_rst_dt/cmd/__init__.py
@@ -5,6 +5,6 @@ irit-rst-dt subcommands
 # Author: Eric Kow
 # License: CeCILL-B (French BSD3)
 
-from . import evaluate, features, gather
+from . import evaluate, features, gather, clean
 
-SUBCOMMANDS = [gather, evaluate, features]
+SUBCOMMANDS = [gather, evaluate, features, clean]
