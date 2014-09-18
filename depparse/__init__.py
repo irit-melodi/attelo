a63cf0b45145f718a2f2b77b56bc1885dadb4b86
Eric Kow <eric.kow@gmail.com>
2014-09-18 14:19:19 +0100

    Depend on the the new depparse package
    No more carrying it around in our own repo, huzzah!
    Thanks, (depparse author) Leif!

--- depparse/__init__.py
+++
@@ -1,5 +0,0 @@
-from mst import Ruleset as MstRuleset
-from mst import Parser as MstParser
-from perceptron import VotedPerceptron
-from sentence import sentences_from_conll
-from parser import Evaluator
