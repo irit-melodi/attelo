f182f543570eeaae295fba55d0c8cc51ab19be67
Eric Kow <eric.kow@gmail.com>
2014-12-24 17:25:36 +0000

    Fix packages and requires in setup.py
    Closes #1
    
    One reason dependencies may not have been installed by the way is
    that I had gotten the field wrong. It's *install_requires*.
    
    Not sure where I got requires from

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
-from setuptools import setup
+'''
+irit-rst-dt setup: irit-rst-dt is an experiment harness.
+
+Its job is to reduce complex experiment pipelines to simple
+commands like `irit-rst-dt gather` and `irit-rst-dt evaluate`
+'''
+
+from setuptools import setup, find_packages
 import glob
 import os
 
@@ -6,7 +13,6 @@ setup(name='irit-rst-dt',
       version='0.1',
       author='Eric Kow',
       author_email='eric@erickow.com',
-      packages=[],
+      packages=find_packages(),
       scripts=[f for f in glob.glob('scripts/*') if not os.path.isdir(f)],
-      requires=['educe', 'attelo']
-      )
+      install_requires=['educe', 'attelo'])
