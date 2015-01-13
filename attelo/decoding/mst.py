2c4b78c57389e75971970f6347e35bc1c71cec3f
jrmyp <perret@irit.fr>
2015-01-13 14:48:38 +0100

    MSDAG support (not used anywhere yet)

--- attelo/decoding/mst.py
+++ attelo/decoding/mst.py
@@ -1,17 +1,58 @@
 '''
 Created on Jun 27, 2012
 
-@author: stergos
+@author: stergos, jrmyp
 
 
 '''
 
 from collections import defaultdict
 from math import log
+from functools import wraps
 import sys
+
 
 from depparse.graph import Digraph
 
+class DigraphPlus(Digraph):
+    """ Extenstion of Digraph, with MSDAG """
+    
+    def __init__(self, *a, **kw):
+        """ Initialize graph """
+        super(DigraphPlus, self).__init__(*a, **kw)
+    
+    @classmethod
+    def clone(cls, dg):
+        return cls(dg.successors, dg.get_score, dg.get_label)
+        
+    def msdag(self):
+        """ Return the MSDAG of the graph using a variant of CLE
+        
+        Returns a new DigraphPlus
+        """
+        mark = Digraph.new_node_id
+        # Consider all edges instead of subgraph
+        candidate = self
+        cycle = candidate.find_cycle()
+        if not cycle:
+            return candidate
+        new_id, old_edges, compact = self.contract(cycle)
+        merged = self.merge(compact.msdag(), new_id, old_edges, cycle)
+        return merged
+
+    def find_cycle(self, *a, **ka):
+        """ Similar to Digraph.find_cycle, but returns a DigraphPlus """
+        res = super(DigraphPlus, self).find_cycle(*a, **ka)
+        return res if (res is None) else DigraphPlus.clone(res)
+
+    def merge(self, *a, **ka):
+        """ Similar to Digraph.merge, but returns a DigraphPlus """
+        return DigraphPlus.clone(super(DigraphPlus, self).merge(*a, **ka))
+
+    def contract(self, *a, **ka):
+        """ Similar to Digraph.contract, but returns a DigraphPlus """
+        new_id, old_edges, dg = super(DigraphPlus, self).contract(*a, **ka)
+        return new_id, old_edges, DigraphPlus.clone(dg)
 
 def get_root(_):
     """ This is used for the construction of the graph. Since we are
@@ -33,7 +74,7 @@ def _graph(instances, root='1', use_prob=True):
         root is the "root" of the graph, that is the node that has no incoming
         nodes
 
-        returns the Maximum Spanning Tree
+        returns the Graph
     """
 
     targets = defaultdict(list)
@@ -53,10 +94,10 @@ def _graph(instances, root='1', use_prob=True):
 
     return Digraph(targets,
                    lambda s, t: scores[s, t],
-                   lambda s, t: labels[s, t]).mst()
+                   lambda s, t: labels[s, t])
 
 
-def _list_edges(instances, root='1', use_prob=True):
+def _list_edges(instances, root='1', use_prob=True, use_msdag=False):
     """ instances are quadruplets of the form:
 
             source, target, probability_of_attachment, relation
@@ -64,12 +105,13 @@ def _list_edges(instances, root='1', use_prob=True):
         root is the "root" of the graph, that is the node that has no incoming
         nodes
 
-        returns a list of edges for the MST graph
+        returns a list of edges for the MST/MSDAG graph
     """
-    mst = _graph(instances, root, use_prob=use_prob)
+    graph = _graph(instances, root, use_prob=use_prob)
+    subgraph = graph.msdag() if use_msdag else graph.mst()
 
-    return [(src, tgt, mst.get_label(src, tgt))
-            for src, tgt in mst.iteredges()]
+    return [(src, tgt, subgraph.get_label(src, tgt))
+            for src, tgt in subgraph.iteredges()]
 
 
 def mst_decoder(*args, **kwargs):
@@ -77,3 +119,7 @@ def mst_decoder(*args, **kwargs):
     maximum spanning tree of the original
     """
     return _list_edges(*args, **kwargs)
+
+def msdag_decoder(*args, **kwargs):
+    """ Attach according to MSDAG (subgraph of original) """
+    return _list_edges(*args, use_msdag=True, **kwargs)
