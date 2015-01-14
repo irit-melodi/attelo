irit-melodi/attelo

commit 424f4fd0f8107cc9ff73661c8ecb244b1e97d573

FIX: crash on edge case where nothing is attached

attelo/decoding/control.py
@@ -165,8 +165,9 @@ def _get_attach_prob_orange(config, attach):
     prob_distrib = []
     for inst in attach.data:
         edu1, edu2 = edu_pair(inst)
-        probs = attach.model(inst, Classifier.GetProbabilities)[1]
-        prob_distrib.append((edu1, edu2, probs, "unlabelled"))
+        dist = attach.model(inst, Classifier.GetProbabilities)
+        prob = dist['True'] if 'True' in dist else 0.
+        prob_distrib.append((edu1, edu2, prob, "unlabelled"))
     return prob_distrib
 
 

attelo/table.py
@@ -13,18 +13,35 @@ def AtteloTableException(Exception):
         super(AtteloTableException, self).__init__(msg)
 
 
+def _related_condition(table, key, value):
+    """
+    Return a condition for `table.filter` that picks out instances
+    for which the value associated with `key` is `value`.
+
+    This would be just `{key: value}` except for the edge case where
+    `value` is not in the table
+    """
+    if value in table.domain[key].values:
+        cond_values = [value]
+    else:
+        cond_values = []
+    return {key: cond_values}
+
+
 def related_attachments(phrasebook, table):
     """Return just the entries in the attachments table that
     represent related EDU pairs
     """
-    return table.filter_ref({phrasebook.label: "True"})
+    cond = _related_condition(table, phrasebook.label, 'True')
+    return table.filter_ref(cond)
 
 
 def related_relations(phrasebook, table):
     """Return just the entries in the relations table that represent
     related EDU pair
     """
-    return table.filter_ref({phrasebook.label: [UNRELATED]}, negate=1)
+    cond = _related_condition(table, phrasebook.label, UNRELATED)
+    return table.filter_ref(cond, negate=1)
 
 
 def _subtable_in_grouping(phrasebook, grouping, table):

commit 59f68bdfd97e5a45bac275e8f4eb750da91ef8ae

Fix regression introduced by 424f4fd0
This was causing us to always return probabilities of 0,
which in turn caused us to get zero scores everywhere.

If I understand correctly, Orange distributions behave like dictionaries
for indexing but not for things like `in` keyword.

This is a sort of type error, I think, and if so, SIGH

attelo/decoding/control.py
@@ -81,7 +81,7 @@ def _get_inst_attach_orange(model, inst):
     :rtype: float
     """
     dist = model(inst, Classifier.GetProbabilities)
-    return dist['True'] if 'True' in dist else 0.
+    return dist['True'] if 'True' in dist.keys() else 0.
 
 
 def _get_inst_relate_orange(model, inst):