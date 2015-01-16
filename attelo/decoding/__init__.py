72888abece9e6edb7583edb17b1b4a1c26ca638a
Eric Kow <eric.kow@gmail.com>
2015-01-16 10:26:31 +0100

    Document attelo packages a little bit more

--- attelo/decoding/__init__.py
+++ attelo/decoding/__init__.py
@@ -1,1 +1,17 @@
+'''
+Decoding in attelo consists in building discourse graphs from a
+set of attachment/labelling predictions.
+
+There are two kinds of modules in this package:
+
+    * decoders: eg., baseline, greedy, mst: convert probability distributions
+      into graphs
+
+    * management: by rights the important bits of these should have been
+      re-exported here so you'd never need to look into attelo.decoding.control
+      or attelo.decoding.util unless you were writing a decoder yourself
+'''
+
+# pylint: disable=wildcard-import
+# (just for re-export)
 from .control import *
