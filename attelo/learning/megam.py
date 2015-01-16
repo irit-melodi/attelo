73ff8c51aa4a2f830676364b0a2207a1b8f4e78c
Eric Kow <eric.kow@gmail.com>
2015-01-16 08:12:24 +0100

    attelo.learning.megam: tidy up, kill dead code

--- attelo/learning/megam.py
+++ attelo/learning/megam.py
@@ -1,6 +1,3 @@
-
-
-
 """
 use nltk megam wrapper for relation prediction.
 
@@ -8,9 +5,14 @@ for that, wrap the wrapper for orange consumption
 
 TODO: render this independent from class name
 """
+
+from __future__ import print_function
+
 import sys
 
 import Orange
+
+# pylint: disable=too-few-public-methods
 
 
 # nice trick from http://stackoverflow.com/a/6796752
@@ -23,6 +25,8 @@ class RedirectStdStreams(object):
     def __init__(self, stdout=None, stderr=None):
         self._stdout = stdout or sys.stdout
         self._stderr = stderr or sys.stderr
+        self.old_stdout = None
+        self.old_stderr = None
 
     def __enter__(self):
         self.old_stdout, self.old_stderr = sys.stdout, sys.stderr
@@ -42,70 +46,77 @@ try:
     import nltk.classify.maxent as maxent
     with RedirectStdStreams(stdout=sys.stderr):
         megam.config_megam()
-    #megam.config_megam(bin="path/to/megam")
-    use_megam = True
-except:
-    print >> sys.stderr, "ERROR: megam not found, configure it before using this"
-    print >> sys.stderr, "using default nltk  maxent"
-    use_megam = False
+    USE_MEGAM = True
+except LookupError:
+    print("ERROR: megam not found, configure it before using this",
+          file=sys.stderr)
+    print("using default nltk maxent", file=sys.stderr)
+    USE_MEGAM = False
 
 
-class MaxentLearner:
+class MaxentLearner(object):
     """
     wrapper to nltk version of max entropy classifier
     TODO: forces to use megam, could use a parameter for other algorithms.
 
     return_type: whether to return the "label" (a string) or an orange "value"
     """
-    def __init__(self, name = 'maxent', return_type = "label"):
+    def __init__(self, name='maxent'):
         self.name = name
 
-    def __call__(self, data, weight = None):
-        conv_data = map(orange2maxent, data)
-        if use_megam:
-            model = maxent.MaxentClassifier.train(conv_data, algorithm = "megam")
+    def __call__(self, data, weight=None):
+        conv_data = [orange2maxent(x) for x in data]
+        if USE_MEGAM:
+            model = maxent.MaxentClassifier.train(conv_data, algorithm="megam")
         else:
             model = maxent.MaxentClassifier.train(conv_data)
-        return MaxentClassifier(classifier = model, domain = data.domain, name=self.name)#["CLASS"].values)
+        return MaxentClassifier(classifier=model,
+                                domain=data.domain,
+                                name=self.name)
+
 
 # TODO: probdist will calculate prob even for classes filtered out
-# (eg UNRELATED), because they are still in the domain. 
+# (eg UNRELATED), because they are still in the domain.
 # this is safer this way, as it ensures probs are stored in orange order
 # and attachment classifier assumes fixed order (should be changed!)
-# not a problem for relations, though. 
-class MaxentClassifier:
+# not a problem for relations, though.
+class MaxentClassifier(object):
+    '''Orange-compatible classifier wrapping around the NLTK interface
+    to maxent/megam classifiers
+    '''
 
-    def __init__(self, name="maxent",**kwds):
+    def __init__(self, name="maxent", **kwds):
         self.__dict__.update(kwds)
-        self.name=name
+        self.name = name
 
-    #def __call__(self, instance, result_type):
     def __call__(self, *args):
-        if len(args)==1:
+        if len(args) == 1:
             instance = args[0]
             result_type = Orange.classification.Classifier.GetValue
         else:
-             instance,result_type = args
-  
+            instance, result_type = args
+
         test = orange2maxent(instance)[0]
+        # pylint: disable=no-member
+        # ugh: these members come in through the kwds arguments
         label = self.classifier.classify(test)
-        v = Orange.data.Value(self.domain.classVar, label)
+        value = Orange.data.Value(self.domain.classVar, label)
 
         prob_dist = self.classifier.prob_classify(test)
 
         if result_type == Orange.classification.Classifier.GetValue:
-            return v#label
+            return value  # label
         elif result_type == Orange.classification.Classifier.GetProbabilities:
             return {x: prob_dist.prob(x) for x in self.domain.classVar.values}
         else:
-            return v, [prob_dist.prob(x) for x in self.domain.classVar.values]
-
-    #def __call__(self,instance):
-    #    return self(instance,Orange.classification.Classifier.GetValue)
+            dist = [prob_dist.prob(x) for x in self.domain.classVar.values]
+            return value, dist
+        # pylint: enable=no-member
 
 
 def orange2maxent(example):
     """convert orange example to featureset+label for maxent API"""
-    featlist = [(f.variable.name, f.value) for f in example.native() if f.variable.name != "CLASS"]
+    feats = {f.variable.name: f.value
+             for f in example.native() if f.variable.name != "CLASS"}
     label = example.get_class().value
-    return dict(featlist), label
+    return feats, label
