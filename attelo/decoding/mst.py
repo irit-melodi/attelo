a154867d6be0f1d91a97c41cf21a7865c3144420
jrmyp <perret@irit.fr>
2015-01-23 14:13:47 +0100

    MSDAG support, MST fix
    Removed Digraph class (which didn't work).
    
    The root node is now set to the first node by span order.
    This replaces the hard-coded Annodis id.
    
    Graph edge weight has been shifted from log(prob) to logit(prob)
    Logit is the inverse sigmoid function and represents confidence about
    attachement more accurately.
    As the depparse implementation of MST sets a lower bound on weights,
    we cut off extreme weights (obtained for limit cases prob=0 and prob=1).
    
    Quick tests have been provided for MST and MSDAG.
    More tests will probably be added for completeness.
    
    MSDAG decoder has been added to the available decoders list.

--- attelo/decoding/mst.py
+++ attelo/decoding/mst.py
@@ -2,116 +2,94 @@
 Created on Jun 27, 2012
 
 @author: stergos, jrmyp
-
-
 '''
 
-from collections import defaultdict
-from math import log
-from functools import wraps
+from __future__ import print_function
 import sys
 
+from collections import defaultdict
+from scipy.special import logit
 
 from depparse.graph import Digraph
 from .interface import Decoder
 
 # pylint: disable=too-few-public-methods
 
-class DigraphPlus(Digraph):
-    """ Extenstion of Digraph, with MSDAG """
-    
-    def __init__(self, *a, **kw):
-        """ Initialize graph """
-        super(DigraphPlus, self).__init__(*a, **kw)
-    
-    @classmethod
-    def clone(cls, dg):
-        return cls(dg.successors, dg.get_score, dg.get_label)
-        
-    def msdag(self):
-        """ Return the MSDAG of the graph using a variant of CLE
-        
-        Returns a new DigraphPlus
-        """
-        mark = Digraph.new_node_id
-        # Consider all edges instead of subgraph
-        candidate = self
-        cycle = candidate.find_cycle()
-        if not cycle:
-            return candidate
-        new_id, old_edges, compact = self.contract(cycle)
-        merged = self.merge(compact.msdag(), new_id, old_edges, cycle)
-        return merged
+# Required to work with depparse, working with a "minimum" score of 1e-100
+# Unless we have more than 1e10 nodes, combined scores shouldn't reach it
+MAX_SCORE = 1e90
+MIN_SCORE = -MAX_SCORE
 
-    def find_cycle(self, *a, **ka):
-        """ Similar to Digraph.find_cycle, but returns a DigraphPlus """
-        res = super(DigraphPlus, self).find_cycle(*a, **ka)
-        return res if (res is None) else DigraphPlus.clone(res)
+def _get_root(edus):
+    """ Returns the root node for MST/MSDAG algorithm
 
-    def merge(self, *a, **ka):
-        """ Similar to Digraph.merge, but returns a DigraphPlus """
-        return DigraphPlus.clone(super(DigraphPlus, self).merge(*a, **ka))
-
-    def contract(self, *a, **ka):
-        """ Similar to Digraph.contract, but returns a DigraphPlus """
-        new_id, old_edges, dg = super(DigraphPlus, self).contract(*a, **ka)
-        return new_id, old_edges, DigraphPlus.clone(dg)
-
-def get_root(_):
-    """ This is used for the construction of the graph. Since we are
-    using the Chu-Liu Edmonds algorithm in it's dependency parsing
-    incarnation, there should be a node that is the root, i.e.  a node
-    that has no incoming edges. This function is supposed to return
-    that node. For the moment it always returns the first
-    node. Syntactic analysis should be sufficient to provide the real
-    head node.
+        Currently, the EDU in first position
     """
-    return '1'
+    return sorted(edus, key=lambda e:e.span)[0]
 
+def _graph(instances, use_prob=True):
+    """ Builds a directed graph for instances
 
-def _graph(instances, root='1', use_prob=True):
-    """ instances are quadruplets of the form:
+        instances are quadruplets of the form:
+            edu_source, edu_target, probability_of_attachment, relation
 
-            source, target, probability_of_attachment, relation
-
-        root is the "root" of the graph, that is the node that has no incoming
-        nodes
-
-        returns the Graph
+        returns a Digraph
     """
+    
+    root_id = _get_root(set(e for s,t,_,_ in instances for e in (s,t))).id
 
     targets = defaultdict(list)
     labels = dict()
     scores = dict()
 
     for source, target, prob, rel in instances:
-        src = source.id
-        tgt = target.id
-        if tgt == root:
+        src, tgt = source.id, target.id
+
+        # Ignore all edges directed to the root
+        if tgt == root_id:
             continue
-        scores[src, tgt] = prob
+
+        scores[src, tgt] = min(MAX_SCORE, max(MIN_SCORE, logit(prob))) if use_prob else prob
         labels[src, tgt] = rel
-        if use_prob:  # probability scores
-            scores[src, tgt] = log(prob if prob != 0.0 else sys.float_info.min)
         targets[src].append(tgt)
 
     return Digraph(targets,
                    lambda s, t: scores[s, t],
                    lambda s, t: labels[s, t])
 
+def _msdag(graph):
+    """ Returns a subgraph of graph (a Digraph) corresponding to its
+        Maximum Spanning Directed Acyclic Graph
 
-def _list_edges(instances, root='1', use_prob=True, use_msdag=False):
-    """ instances are quadruplets of the form:
-
-            source, target, probability_of_attachment, relation
-
-        root is the "root" of the graph, that is the node that has no incoming
-        nodes
-
-        returns a list of edges for the MST/MSDAG graph
+        Alogrithm is semi-greedy-MSDAG as described in Schluter (2014)
     """
-    graph = _graph(instances, root, use_prob=use_prob)
-    subgraph = graph.msdag() if use_msdag else graph.mst()
+    tree = graph.mst()
+    # Sort edges in orginal graph by decreasing score
+    edges = sorted(graph.iteredges(), key=lambda p:-graph.get_score(*p))
+    for src, tgt in edges:
+        # Already in graph ?
+        if tgt in tree.successors[src]:
+            continue
+        # Add the edge, revert if cycle is created
+        tree.successors[src].append(tgt)
+        if tree.find_cycle():
+            tree.successors[src].remove(tgt)
+
+    # Update score and label functions
+    # I really wish that Digraph used a pair as single argument,
+    #   the following would be clearer
+    nscores, nlabels = (dict((k, old(*k)) for k in tree.iteredges())
+                        for old in (graph.get_score, graph.get_label))
+
+    return Digraph(tree.successors,
+                   lambda s, t: nscores[s, t],
+                   lambda s, t: nlabels[s, t])
+
+def _list_edges(instances, use_prob=True, use_msdag=False):
+    """ Returns a list of edges for the MST/MSDAG graph """
+    
+    graph = _graph(instances, use_prob=use_prob)
+    subgraph = _msdag(graph) if use_msdag else graph.mst()
 
     return [(src, tgt, subgraph.get_label(src, tgt))
             for src, tgt in subgraph.iteredges()]
@@ -120,16 +98,19 @@ class MstDecoder(Decoder):
     """ attach in such a way that the resulting subgraph is a
     maximum spanning tree of the original
     """
-    def __init__(self, root='1', use_prob=True):
-        self._root = root
+    def __init__(self, use_prob=True):
         self._use_prob = use_prob
 
     def decode(self, instances):
-        prediction = _list_edges(instances, self._root, self._use_prob)
+        prediction = _list_edges(instances, self._use_prob)
         return [prediction]
 
-    return _list_edges(*args, **kwargs)
+class MsdagDecoder(Decoder):
+    """ Attach according to MSDAG (subgraph of original)"""
+    
+    def __init__(self, use_prob=True):
+        self._use_prob = use_prob
 
-def msdag_decoder(*args, **kwargs):
-    """ Attach according to MSDAG (subgraph of original) """
-    return _list_edges(*args, use_msdag=True, **kwargs)
+    def decode(self, instances):
+        prediction = _list_edges(instances, self._use_prob, True)
+        return [prediction]
