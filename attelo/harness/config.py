80ffa55fba9bab06479cf8b46c4363a796d92c1f
Eric Kow <eric.kow@gmail.com>
2015-02-06 14:57:31 +0100

    ENH simpler attelo.harness.config
    The idea:
    - learners and decoders now have the same structure
      to represent their configuration (Variant)
    - each variant has a key (your responsibility to
      generate a unique one), the name, and a list of
      command line flags relevant to its configuration
    - to represent the attach/relate learner pair, just
      use attelo.util.Team

--- attelo/harness/config.py
+++ attelo/harness/config.py
@@ -8,6 +8,7 @@ Configuring the harness
 import argparse
 from collections import namedtuple
 
+# pylint: disable=too-few-public-methods
 
 # pylint: disable=abstract-class-not-used
 class CliArgs(argparse.Namespace):
@@ -73,48 +74,28 @@ class CliArgs(argparse.Namespace):
         return
 
 
-# pylint: disable=pointless-string-statement, too-few-public-methods
-class LearnerConfig(object):
+class Variant(namedtuple("Variant", "key name flags")):
     """
-    Unique combination of learner settings.
+    Unique combination of settings for either a learner or
+    decoder
 
-    It is crucial that each configuration be associated with
-    a unique name, as we use these names to determine if we can
-    reuse a learner or not.
-    """
-    def __init__(self, name, attach, relate=None):
-        self.name = name
-        "unique name for this configuration"
+    :param name: name of the learner/decoder
+    :type name: string
 
-        self.attach = attach
-        "attachment learner"
+    :param key: unique string that distinguishes this learner/decoder
+                from others (eg. 'astar-2-best')
+    :type key: string
 
-        self.relate = relate
-        "relation learner (None if same as attach)"
-
-    @classmethod
-    def simple(cls, name):
-        """
-        Simple learner consisting of just the attachment learner
-        of the same name.
-        """
-        return cls(name, name)
-
-
-_DecoderConfig = namedtuple("_DecoderConfig", "name decoder")
-
-
-class DecoderConfig(_DecoderConfig):
-    """
-    Unique combination of decoder settings.
+    :param flags: command line flags
+    :type flags: [string]
     """
     @classmethod
     def simple(cls, name):
         """
-        Simple learner consisting of just the decoder of the same
-        name.
+        Simple variant where the key is just the name of component
+        itself
         """
         return cls(name, name)
-# pylint: enable=pointless-string-statement, too-few-public-methods
+
 
 EvaluationConfig = namedtuple("EvaluationConfig", "name learner decoder")
