a60e6f15ebc427c184c9d9ec953719d4834c4323
Eric Kow <eric.kow@gmail.com>
2015-02-10 11:22:04 +0100

    MAINT remove obsolete CountIndex code

--- attelo/harness/report.py
+++ attelo/harness/report.py
@@ -5,41 +5,7 @@ Helpers for result reporting
 # Author: Eric Kow
 # License: CeCILL-B (French BSD3-like)
 
-import csv
-
-from attelo.cmd.report import EXPECTED_KEYS
-
 # pylint: disable=too-few-public-methods
-
-
-# pylint: disable=pointless-string-statement, too-few-public-methods
-class CountIndex(object):
-    """
-    Convenience wrapper to generate attelo count index files.
-    Any exceptions raised will be bubbled up
-
-    with CountIndex(path) as writer:
-        writer.writerow(foo)
-
-    """
-    def __init__(self, path):
-        self.path = path
-        "path to the wrapped count file"
-
-        self._stream = None
-        self._writer = None
-
-    def __enter__(self):
-        self._stream = open(self.path, "w")
-        self._writer = csv.DictWriter(self._stream,
-                                      fieldnames=EXPECTED_KEYS)
-        self._writer.writeheader()
-        return self._writer
-
-    def __exit__(self, etype, value, tback):
-        if tback is None:
-            self._stream.close()
-# pylint: enable=pointless-string-statement, too-few-public-methods
 
 
 def mk_index(folds, configurations):
