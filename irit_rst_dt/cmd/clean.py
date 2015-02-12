898a5843e03e807baad0ab7ee55b4b1037e53192
Eric Kow <eric.kow@gmail.com>
2015-02-12 14:09:29 +0100

    FIX make irit-rst-dt clean a bit less aggressive
    It was deleting eval dirs that don't have a scores-*
    directory... except that I'd renamed this to reports-*.
    
    Oops.

--- irit_rst_dt/cmd/clean.py
+++ irit_rst_dt/cmd/clean.py
@@ -47,6 +47,6 @@ def main(_):
             elif bname.startswith("scratch-"):
                 shutil.rmtree(subdir)
             elif bname.startswith("eval-"):
-                if not any(f.startswith("scores-") for
+                if not any(f.startswith("reports-") for
                            f in os.listdir(subdir)):
                     shutil.rmtree(subdir)
