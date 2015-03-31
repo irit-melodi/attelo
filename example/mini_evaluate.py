43d67e2036dbf33a92a70a07977683690b30e376
Eric Kow <eric.kow@gmail.com>
2015-03-31 18:18:06 +0200

    MAINT update example for multipack

--- example/mini_evaluate.py
+++ example/mini_evaluate.py
@@ -12,12 +12,15 @@ from attelo.decoding import (DecodingMode, decode)
 from attelo.decoding.mst import (MstDecoder,
                                  MstRootStrategy)
 from attelo.learning import (learn)
-from attelo.fold import (make_n_fold)
-from attelo.io import (load_data_pack,
+from attelo.fold import (make_n_fold,
+                         select_testing,
+                         select_training)
+from attelo.io import (load_multipack,
                        write_predictions_output)
 from attelo.report import (CombinedReport,
                            EdgeReport)
 from attelo.score import (score_edges)
+from attelo.table import (DataPack)
 from attelo.util import (mk_rng, Team)
 from sklearn.linear_model import (LogisticRegression)
 
@@ -31,16 +34,14 @@ if not fp.exists(TMP_OUTPUT):
     os.makedirs(TMP_OUTPUT)
 
 # load the data
-dpack = load_data_pack(PREFIX + '.edus',
+mpack = load_multipack(PREFIX + '.edus',
                        PREFIX + '.pairings',
                        PREFIX + '.features.sparse',
                        verbose=True)
 
 # divide the dataset into folds
-num_groupings = len(set(e.grouping for e in dpack.edus
-                        if e.grouping is not None))
-num_folds = min((10, num_groupings))
-fold_dict = make_n_fold(dpack, num_folds, mk_rng())
+num_folds = min((10, len(mpack)))
+fold_dict = make_n_fold(mpack, num_folds, mk_rng())
 
 # select a decoder and a learner team
 decoder = MstDecoder(root_strategy=MstRootStrategy.fake_root)
@@ -53,18 +54,18 @@ for fold in range(num_folds):
     print(">>> doing fold ", fold + 1, file=sys.stderr)
     print("training ... ", file=sys.stderr)
     # learn a model for the training data for this fold
-    train_pack = dpack.training(fold_dict, fold)
+    train_pack = select_training(mpack, fold_dict, fold)
     models = learn(train_pack, learners)
 
     fold_predictions = []
     # decode each document separately
-    for onedoc, indices in dpack.groupings().items():
+    test_pack = select_testing(mpack, fold_dict, fold)
+    for onedoc, dpack in test_pack.items():
         print("decoding on file : ", onedoc, file=sys.stderr)
-        test_pack = dpack.selected(indices)
-        predictions = decode(test_pack, models, decoder,
+        predictions = decode(dpack, models, decoder,
                              DecodingMode.joint)
         # record the best prediction score (among the decoder nbest)
-        doc_scores = [score_edges(test_pack, x) for x in predictions]
+        doc_scores = [score_edges(dpack, x) for x in predictions]
         scores.append(max(doc_scores, key=lambda x: x.tpos_label))
         # optional: save the predictions for further inspection
         fold_predictions.extend(predictions[0])
@@ -72,7 +73,8 @@ for fold in range(num_folds):
     # optional: write predictions for this fold
     output_file = fp.join(TMP_OUTPUT, 'fold-%d' % fold)
     print("writing: %s" % output_file, file=sys.stderr)
-    write_predictions_output(dpack, fold_predictions, output_file)
+    write_predictions_output(DataPack.vstack(test_pack.values()),
+                             fold_predictions, output_file)
 
 report = EdgeReport(scores)
 
