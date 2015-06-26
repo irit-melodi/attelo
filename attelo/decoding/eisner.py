6236495cdaa72e663cbc86a0d63db54c1ca9df29
moreymat <mathieu.morey@gmail.com>
2015-06-26 14:28:35 +0200

    MAINT pep8 and some pylint

--- attelo/decoding/eisner.py
+++ attelo/decoding/eisner.py
@@ -1,8 +1,6 @@
 """Eisner decoder
 """
 
-import operator
-
 import numpy as np
 
 from .interface import Decoder
@@ -37,7 +35,7 @@ class EisnerDecoder(Decoder):
         # get number of EDUs and possible labels
         nb_edus = len(dpack.edus)
         # nb_lbls = dpack.graph.label.shape[0]
-        
+
         # map EDU ids to their index in the document
         edu_id2idx = _edu_positions(dpack)
         # we will need the reverse mapping to produce the result
@@ -83,13 +81,15 @@ class EisnerDecoder(Decoder):
                 if start == 0 or (end, start) not in score:
                     cscores[start][end][1][0] = np.NINF
                 else:
-                    cscores[start][end][1][0] = max_split_score + score[(end, start)]
+                    cscores[start][end][1][0] = (max_split_score +
+                                                 score[(end, start)])
                     csplits[start][end][1][0] = best_split_point
                 # right attachment
                 if (start, end) not in score:
                     cscores[start][end][0][0] = np.NINF
                 else:
-                    cscores[start][end][0][0] = max_split_score + score[(start, end)]
+                    cscores[start][end][0][0] = (max_split_score +
+                                                 score[(start, end)])
                     csplits[start][end][0][0] = best_split_point
 
                 # best "closed"
@@ -99,7 +99,8 @@ class EisnerDecoder(Decoder):
                                      cscores[k][end][1][0])
                                     for k in range(start, end)]
                     max_split_score = max(split_scores)
-                    best_split_point = start + split_scores.index(max_split_score)
+                    best_split_point = (start +
+                                        split_scores.index(max_split_score))
                     cscores[start][end][1][1] = max_split_score
                     csplits[start][end][1][1] = best_split_point
                 # right attachment
@@ -107,14 +108,13 @@ class EisnerDecoder(Decoder):
                                  cscores[k][end][0][1])
                                 for k in range(start + 1, end + 1)]
                 max_split_score = max(split_scores)
-                best_split_point = start + 1 + split_scores.index(max_split_score)
+                best_split_point = (start + 1 +
+                                    split_scores.index(max_split_score))
                 cscores[start][end][0][1] = max_split_score
                 csplits[start][end][0][1] = best_split_point
 
-        # solution: C[0][n][->][1]
-        best_score = cscores[0][nb_edus - 1][0][1]
-        best_split = csplits[0][nb_edus - 1][0][1]
 
+        # solution: C[0][n][->][1]
         # use the backpointers in csplits to get the best tree
         predictions = []
         backpointers = [(0, nb_edus - 1, 0, 1)]
@@ -145,7 +145,7 @@ class EisnerDecoder(Decoder):
         # transform predictions to the expected format
         # back to EDU ids and relation labels as strings
         att_preds = [(edu_idx2id[src], edu_idx2id[tgt], dpack.get_label(lbl))
-                      for src, tgt, lbl in predictions]
+                     for src, tgt, lbl in predictions]
         # and integrate predictions into the datapack
         dpack_pred = convert_prediction(dpack, att_preds)
 
